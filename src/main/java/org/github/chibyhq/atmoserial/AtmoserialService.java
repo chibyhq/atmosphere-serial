faeca3872593247fb10fdff20649082841cca65d
Brice Copy <brice.copy@cern.ch>
2018-10-03 12:16:27 +0200

    initial commit

---
+++ src/main/java/org/github/chibyhq/atmoserial/AtmoserialService.java
@@ -0,0 +1,11 @@
+package org.github.chibyhq.atmoserial;
+
+import org.atmosphere.config.service.ManagedService;
+
+@ManagedService
+public class AtmoserialService {
+
+    // Open a port via REST
+    // Start broadcasting the port output
+    
+}
